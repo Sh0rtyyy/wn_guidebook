0a557ad7c3742d1d3ce044f2521e0b1c22aba44a
Shorty <94754507+Sh0rtyyy@users.noreply.github.com>
2024-02-01 20:54:47 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,21 +5,31 @@ Config.Guidebook = {
     {
         title = "How to buy phone ?",
         message = "You need to go to the 24/7.",
-        job = nil,
+        job = nil, -- nil without any job restriction, or "job_name" for restriction
+        gang = nil, -- nil without any gang restriction, or "gang_name" for restriction !!! QB ONLY !!! 
     },   
     {
         title = "How to buy a car ?",
         message = "There is a car dealer on the 6541 postal.",
-        job = nil,
+        job = nil, -- nil without any job restriction, or "job_name" for restriction
+        gang = nil, -- nil without any gang restriction, or "gang_name" for restriction !!! QB ONLY !!! 
+    },
+    {
+        title = "How to make a weed ?",
+        message = "You need to find some seeds. Place them into the ground or pot. Make them feel good and just wait until they grow.",
+        job = nil, -- nil without any job restriction, or "job_name" for restriction
+        gang = "ballas", -- nil without any gang restriction, or "gang_name" for restriction !!! QB ONLY !!! 
     },
     {
         title = "What to do, when a preson is bleading ?",
         message = "Stay Calm: Keep yourself composed to provide effective assistance. Call for Help: Dial emergency services immediately for professional assistance. Protect Yourself: Use gloves or any available barrier to protect yourself from bloodborne pathogens. Apply Pressure: Use a clean cloth or bandage to apply direct pressure on the wound. Elevate the Wound: If possible, elevate the bleeding area above the level of the heart. Maintain Pressure: Continue applying pressure until help arrives or bleeding stops",
-        job = "ambulance",
+        job = "ambulance", -- nil without any job restriction, or "job_name" for restriction
+        gang = nil, -- nil without any gang restriction, or "gang_name" for restriction !!! QB ONLY !!! 
     },
     {
         title = "Whats the miranda ?",
         message = "You have the right to remain silent. Anything you say can and will be used against you in a court of law. You have the right to an attorney. If you cannot afford one, one will be appointed to you by the court. Do you understand your rights ?",
-        job = "police",
+        job = "police", -- nil without any job restriction, or "job_name" for restriction
+        gang = nil, -- nil without any gang restriction, or "gang_name" for restriction !!! QB ONLY !!! 
     },
 }
